5265f70dfbef92c3fdb37376648373aaa04a560b
Vyacheslav Kondratyuk <v.kondratyuk@ism-ukraine.com>
2015-03-17 09:12:34 +0200

    Big accuracy

--- 11.py
+++ 11.py
@@ -1,37 +1,56 @@
-'''
-var
-n,m,k,i,dig:integer;
+def pascalSolution(m, n, k):
+    r = ''
+    dig = m / n
+    r += str(dig)
+    for i in range(1, k + 1):
+        if i == 1: r += '.'
+        m = (m - n * dig) * 10
+        dig = m / n
+        r += str(dig)
+    return r
+    
+from decimal import *
+m, n, k = map(int, input().split())
+#m, n, k = map(int, '10 2 2'.split())
 
-begin
-  read(n,m,k);
-  dig:=n div m;
-  write(dig);
-  for i:=1 to k do
-  begin
-      if i=1 then write('.');
-      n:=(n-m*dig)*10;
-      dig:=n div m;
-      write(dig);
-  end;
-  writeln;
-end.
-'''
+def bigAccuracy(m, n, k):
+    getcontext().prec = k + 2
+    res = str(Decimal(m) / Decimal(n))
 
+    if '.' not in res: res += '.'    
+    spl = res.split('.')
+    spl[1] = spl[1][:k]
+    
+    llen, rlen = len(spl[0]), len(spl[1])
+    
+    if k > 0:
+        zAdd = llen + k + 1
+        res = spl[0] + '.' + spl[1]
+    else:
+        zAdd = llen + k
+        res = spl[0]
+    
+    return res.ljust(zAdd, '0')
 
-#m, n, k = map(int, input().split())
-m, n, k = map(int, '5 289 10'.split())
+print(bigAccuracy(m, n, k))
 
-res = str(m / float(n))
-spl = res.split('.')
-spl[1] = spl[1][:k]
-
-llen, rlen = len(spl[0]), len(spl[1])
-
-if k > 0:
-    add = llen + 1 + k
-    res = spl[0] + '.' + spl[1]
-else:
-    add = llen + k
-    res = spl[0]
-
-print(res.ljust(add, '0'))
+def test():
+    t = (
+        (1, 2, 3),
+        (1, 3, 26),    
+        (5, 289, 10),
+        (11, 12, 26),
+        (50, 4, 3),
+        (18, 111, 7),
+        (10, 2, 2),        
+        (1, 2, 2),        
+        (100, 100, 1000),
+        (22, 7, 1000),
+    )
+    #t = ((10,2,2),)
+    for i in t:
+        print('m=' + str(i[0]) + ' n=' + str(i[1]) + ' k=' + str(i[2]))
+        print('our: ' + bigAccuracy(i[0], i[1], i[2]))
+        print('pas: ' + pascalSolution(i[0], i[1], i[2]))
+        
+test()    
